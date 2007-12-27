5e4d122914b1d21d4df5538a55c9a4ed17bded52
Bryan O'Sullivan <bos@serpentine.com>
2007-12-27 03:41:28 +0000

    Basic types.
    --HG--
    extra : convert_revision : 2ee2b6f5e0d07e7c4166919d1e004fd4145807ae

--- LLVM.hs
+++ LLVM.hs
@@ -8,6 +8,19 @@ module LLVM
     , deleteTypeName
     , getElementType
 
+    , int1Type
+    , int8Type
+    , int16Type
+    , int32Type
+    , int64Type
+    , integerType
+
+    , floatType
+    , doubleType
+    , x86FP80Type
+    , fp128Type
+    , ppcFP128Type
+
     , Value
     , addGlobal
     , setInitializer
@@ -20,6 +33,7 @@ import Foreign.C.String (withCString)
 import Foreign.ForeignPtr (ForeignPtr, FinalizerPtr, newForeignPtr,
                            withForeignPtr)
 import Foreign.Ptr (Ptr)
+import System.IO.Unsafe (unsafePerformIO)
 
 import qualified LLVM.Base as Base
 
@@ -56,9 +70,43 @@ deleteTypeName mod name =
     withModule mod $ \modPtr ->
       withCString name $ Base.deleteTypeName modPtr
 
-getElementType :: Type -> IO Type
-getElementType typ = Type <$> Base.getElementType (fromType typ)
+getElementType :: Type -> Type
+getElementType typ = unsafePerformIO $
+    Type <$> Base.getElementType (fromType typ)
 
+int1Type :: Type
+int1Type = unsafePerformIO $ Type <$> Base.int1Type
+
+int8Type :: Type
+int8Type = unsafePerformIO $ Type <$> Base.int8Type
+
+int16Type :: Type
+int16Type = unsafePerformIO $ Type <$> Base.int16Type
+
+int32Type :: Type
+int32Type = unsafePerformIO $ Type <$> Base.int32Type
+
+int64Type :: Type
+int64Type = unsafePerformIO $ Type <$> Base.int64Type
+
+integerType :: Int -> Type
+integerType width = unsafePerformIO $
+    Type <$> Base.integerType (fromIntegral width)
+
+floatType :: Type
+floatType = unsafePerformIO $ Type <$> Base.floatType
+
+doubleType :: Type
+doubleType = unsafePerformIO $ Type <$> Base.doubleType
+
+x86FP80Type :: Type
+x86FP80Type = unsafePerformIO $ Type <$> Base.x86FP80Type
+
+fp128Type :: Type
+fp128Type = unsafePerformIO $ Type <$> Base.fp128Type
+
+ppcFP128Type :: Type
+ppcFP128Type = unsafePerformIO $ Type <$> Base.ppcFP128Type
 
 newtype Value = Value {fromValue :: Ptr Base.Value}
 
@@ -72,8 +120,8 @@ setInitializer :: Value -> Value -> IO ()
 setInitializer global const =
     Base.setInitializer (fromValue global) (fromValue const)
 
-typeOf :: Value -> IO Type
-typeOf val = Type <$> Base.typeOf (fromValue val)
+typeOf :: Value -> Type
+typeOf val = unsafePerformIO $ Type <$> Base.typeOf (fromValue val)
 
 getNamedFunction :: String -> IO Value
 getNamedFunction name = withCString name $ fmap Value . Base.getNamedFunction
--- LLVM/Base.hsc
+++ LLVM/Base.hsc
@@ -11,6 +11,19 @@ module LLVM.Base
     , deleteTypeName
     , getElementType
 
+    , int1Type
+    , int8Type
+    , int16Type
+    , int32Type
+    , int64Type
+    , integerType
+
+    , floatType
+    , doubleType
+    , x86FP80Type
+    , fp128Type
+    , ppcFP128Type
+
     , Value
     , addGlobal
     , deleteGlobal
@@ -20,7 +33,7 @@ module LLVM.Base
     ) where
 
 import Foreign.C.String (CString)
-import Foreign.C.Types (CInt)
+import Foreign.C.Types (CInt, CUInt)
 import Foreign.Ptr (Ptr)
 
 
@@ -34,6 +47,29 @@ foreign import ccall unsafe "LLVMDisposeModule" disposeModule
 
 
 data Type
+
+foreign import ccall unsafe "LLVMInt1Type" int1Type :: IO (Ptr Type)
+
+foreign import ccall unsafe "LLVMInt8Type" int8Type :: IO (Ptr Type)
+
+foreign import ccall unsafe "LLVMInt16Type" int16Type :: IO (Ptr Type)
+
+foreign import ccall unsafe "LLVMInt32Type" int32Type :: IO (Ptr Type)
+
+foreign import ccall unsafe "LLVMInt64Type" int64Type :: IO (Ptr Type)
+
+foreign import ccall unsafe "LLVMIntType" integerType
+    :: CUInt -> IO (Ptr Type)
+
+foreign import ccall unsafe "LLVMFloatType" floatType :: IO (Ptr Type)
+
+foreign import ccall unsafe "LLVMDoubleType" doubleType :: IO (Ptr Type)
+
+foreign import ccall unsafe "LLVMX86FP80Type" x86FP80Type :: IO (Ptr Type)
+
+foreign import ccall unsafe "LLVMFP128Type" fp128Type :: IO (Ptr Type)
+
+foreign import ccall unsafe "LLVMPPCFP128Type" ppcFP128Type :: IO (Ptr Type)
 
 foreign import ccall unsafe "LLVMAddTypeName" addTypeName
     :: Ptr Module -> CString -> Ptr Type -> IO CInt
