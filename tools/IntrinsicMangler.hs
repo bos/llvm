21533deb2f0c43f7ab89819ea009ab228a24c0aa
Bryan O'Sullivan <bos@serpentine.com>
2008-01-05 05:18:39 +0000

    Intrinsics
    --HG--
    extra : convert_revision : 88903b4d9e37a9ac1f216dcc0a1f1c80df0d2be1

---
+++ tools/IntrinsicMangler.hs
@@ -0,0 +1,21 @@
+module IntrinsicMangler (main) where
+
+import Control.Monad (forM_)
+import qualified Data.ByteString.Char8 as C
+import Data.Maybe (catMaybes)
+import Text.Regex.Posix ((=~~))
+
+maybeName :: C.ByteString -> Maybe C.ByteString
+maybeName line = do
+  ((_:name:_):_) <- line =~~ "^[ \t]*([a-z0-9_]+),[ \t]*//[ \t]*llvm\\."
+  return name
+
+main :: IO ()
+main = do
+  input <- (catMaybes . map maybeName . C.lines) `fmap` C.getContents
+
+  putStrLn "-- automatically generated file - do not edit!"
+  putStrLn "module LLVM.Core.Intrinsics (Intrinsic(..), toIntrinsic, fromIntrinsic) where"
+  putStrLn "data Intrinsic ="
+  forM_ input $ C.putStrLn . (C.append (C.pack "    | I_"))
+  putStrLn "    deriving (Eq, Ord, Enum, Show)"
