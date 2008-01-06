dfce87600a29177ebfd9a20c39bff4f49f72dade
Bryan O'Sullivan <bos@serpentine.com>
2008-01-06 07:07:40 +0000

    A bit of readability fun.
    --HG--
    extra : convert_revision : 95735292be297381e6e3d27d0ff36a0cbd9ffea8

--- tools/FunctionMangler.hs
+++ tools/FunctionMangler.hs
@@ -1,17 +1,23 @@
---module FunctionMangler (main) where
+module FunctionMangler where
 
 import Control.Monad (forM)
 import Data.Char (isSpace, toLower)
 import Data.List (intercalate, isPrefixOf)
 import Text.Regex.Posix ((=~), (=~~))
 
+dropSpace :: String -> String
+dropSpace = dropWhile isSpace
+
 renameType :: String -> String
 renameType "int" = "CInt"
 renameType "unsigned" = "CUInt"
 renameType "void" = "()"
 renameType "const char *" = "CString"
-renameType s | "LLVM" `isPrefixOf` s = drop 4 s
-             | otherwise = error $ "cannot handle " ++ s
+renameType s | "LLVM" `isPrefixOf` s = pointer $ drop 4 s
+             | otherwise = error $ "cannot handle " ++ show s
+    where pointer p = case reverse p of
+                        ('*':ps) -> ("Ptr "++) . reverse $ dropSpace ps
+                        _ -> p
 
 split :: (a -> Bool) -> [a] -> [[a]]
 split p xs = case break p xs of
@@ -19,23 +25,27 @@ split p xs = case break p xs of
                (s,_) -> [s]
 
 strip :: String -> String
-strip = reverse . dropWhile isSpace . reverse . dropWhile isSpace
+strip = reverse . dropWhile isSpace . reverse . dropSpace
 
 dropName :: String -> String
 dropName s =
-    let ((_:typ:_):_) = s =~ "^((const )?[A-Za-z0-9_]+( \\*)?) ?[A-Za-z0-9]*$"
+    let pattern = "^((const )?[A-Za-z0-9_]+( \\*)?) ?[A-Za-z0-9]*$"
+        ((_:typ:_):_) = s =~ pattern
     in typ
 
-rewrite :: String -> [[String]]
+rewrite :: Monad m => String -> m [String]
 rewrite s = do
-  let pat = "^([A-Za-z0-9_ ]+ ?\\*?)[ \t\n]*LLVM([A-Za-z0-9_]+)\\(([a-zA-Z0-9_*, \t\n]+)\\);"
-  matches <- s =~~ pat
-  forM matches $ \(_:ret:name:params:_) -> do
-    let retType = "IO " ++ renameType ret
-        ps = map (renameType . dropName . strip) . split (==',') $ params
-        hsName = let (n:ame) = name in toLower n : ame
-    return $ "foreign import ccall unsafe \"LLVM" ++ name ++ "\" " ++ hsName ++
-             "\n    :: " ++ intercalate " -> " (ps ++ [retType])
+    let pattern = "^([A-Za-z0-9_ ]+ ?\\*?)[ \t\n]*" ++
+                  "LLVM([A-Za-z0-9_]+)\\(([a-zA-Z0-9_*, \t\n]+)\\);"
+    matches <- s =~~ pattern
+    forM matches $ \(_:cret:cname:cparams:_) -> do
+      let ret = "IO " ++ renameType (strip cret)
+          params = map renameParam . split (==',') $ cparams
+          name = let (n:ame) = cname in toLower n : ame
+      return $ foreign ++ "\"LLVM" ++ cname ++ "\" " ++ name ++
+               "\n    :: " ++ intercalate " -> " (params ++ [ret])
+  where renameParam = renameType . dropName . strip
+        foreign = "foreign import ccall unsafe "
 
 main :: IO ()
 main = interact (intercalate "\n\n" . concat . rewrite) >> putStr "\n"
