2b7ac8b3957208aa1b0431b4d663e161c7bacc31
lennart <lennart@augustsson.net>
2008-01-11 23:35:53 +0000

    More Show instances.
    --HG--
    extra : convert_revision : e8f37bcfb64cf956292bad428fdbdb234d6727e2

--- LLVM/Core/Data.hs
+++ LLVM/Core/Data.hs
@@ -10,20 +10,26 @@ import Data.TypeNumbers
 -- |Variable sized signed integer.
 -- The /n/ parameter should belong to @IsTypeNumber@.
 newtype (IsTypeNumber n) => IntN n = IntN Integer
+    deriving (Show)
 
 -- |Variable sized unsigned integer.
 -- The /n/ parameter should belong to @IsTypeNumber@.
 newtype (IsTypeNumber n) => WordN n = WordN Integer
+    deriving (Show)
 
 -- |128 bit floating point.
 newtype FP128 = FP128 Rational
+    deriving (Show)
 
 -- |Fixed sized arrays, the array size is encoded in the /n/ parameter.
 newtype (IsTypeNumber n) => Array n a = Array [a]
+    deriving (Show)
 
 -- XXX Power of 2 size constraint not enforced
 -- |Fixed sized vector, the array size is encoded in the /n/ parameter.
 newtype Vector n a = Vector (Array n a)
+    deriving (Show)
 
 -- |Pointer type.
 newtype Ptr a = Ptr a
+    deriving (Show)
