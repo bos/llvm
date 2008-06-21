8d138d72e35b9670f73855176adb081afaca37d6
lennart <lennart@augustsson.net>
2008-06-21 14:12:37 +0000

    Simplify.
    --HG--
    extra : convert_revision : 14e27d870a235daf3fe79fdf2079b25e8d37b81d

--- examples/HelloJIT.hs
+++ examples/HelloJIT.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE TypeOperators #-}
 module HelloJIT (main) where
 
 import Data.Int(Int32)
@@ -7,30 +6,20 @@ import Data.Word
 import LLVM.Core
 import LLVM.ExecutionEngine
 
-buildModule :: IO (Module, Function (IO ()))
-buildModule = do
-  m <- newNamedModule "hello"
-  func <- defineModule m $ do
+bldGreet :: CodeGenModule (Function (IO ()))
+bldGreet = do
     greetz <- createGlobal InternalLinkage $ constStringNul "Hello, JIT!"
     puts <- newNamedFunction ExternalLinkage "puts" :: TFunction (Ptr Word8 -> IO Word32)
     func <- createFunction ExternalLinkage $ do
       let zero = valueOf (0::Int32)
-      tmp <- getElementPtr greetz [zero, zero]
+      tmp <- getElementPtr greetz [zero::Value Int32, zero]
       call puts tmp -- Throw away return value.
       ret ()
     return func
-  return (m, func)
-
-execute :: Module -> Function (IO ()) -> IO ()
-execute m func = do
-  prov <- createModuleProviderForExistingModule m
-  ee <- createExecutionEngine prov
-  runStaticConstructors ee -- Not needed here.
-  let greet = generateFunction ee func
-  greet
-  greet
-  runStaticDestructors ee -- Not needed here.
-  return ()
 
 main :: IO ()
-main = buildModule >>= uncurry execute
+main = do
+  greet <- simpleFunction bldGreet
+  greet
+  greet
+  return ()
