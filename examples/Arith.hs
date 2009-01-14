763e960adca930ed60e201dd208f8f6b29920ecc
lennart <lennart@augustsson.net>
2009-01-14 08:04:51 +0000

    Add vector example.
    --HG--
    extra : convert_revision : 5cdbe2c67a14bc485e1d85a3ade254bf7e1343cc

--- examples/Arith.hs
+++ examples/Arith.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Arith where
 import Data.Int
+import Data.TypeNumbers
 import LLVM.Core
 import LLVM.ExecutionEngine
 import LLVM.Util.Arithmetic
@@ -11,11 +12,29 @@ mSomeFn :: forall a . (IsConst a, Floating a, IsFloating a, Cmp a,
 	     ) => CodeGenModule (Function (a -> IO a))
 mSomeFn = do
     foo <- createFunction InternalLinkage $ arithFunction $ \ x y -> exp (sin x) + y
-    createFunction ExternalLinkage $ arithFunction $ \ x ->
-        sqrt (x^2 - 5 * x + 6) + toArithFunction foo x x
+    let foo' = toArithFunction foo
+    createFunction ExternalLinkage $ arithFunction $ \ x -> do
+        y <- set $ x^3
+        sqrt (x^2 - 5 * x + 6) + foo' x x + y + log y
 
 mFib :: CodeGenModule (Function (Int32 -> IO Int32))
 mFib = recursiveFunction $ \ rfib n -> n %< 2 ? (1, rfib (n-1) + rfib (n-2))
+
+type V = Vector (D4 End) Float
+
+mVFun :: CodeGenModule (Function (V -> IO V))
+mVFun = do
+    vlogf' <- newNamedFunction ExternalLinkage "vlogf"
+    let vlogf :: TValue r V -> TValue r V
+        vlogf x = toArithFunction vlogf' x
+--        vlogf x = do x' <- x; call vlogf' x'
+    vexpf' <- newNamedFunction ExternalLinkage "vexpf"
+    let vexpf :: TValue r V -> TValue r V
+--	vexpf x = do x' <- x; call vexpf' x'
+        vexpf x = toArithFunction vexpf' x
+    createFunction ExternalLinkage $ arithFunction $ \ x ->
+        vlogf x * vexpf x * x - 16
+
 
 writeFunction :: String -> CodeGenModule a -> IO ()
 writeFunction name f = do
@@ -29,6 +48,8 @@ main = do
     ioSomeFn <- simpleFunction mSomeFn'
     let someFn :: Double -> Double
         someFn = unsafePurify ioSomeFn
+
+    writeFunction "VArith.bc" mVFun
 
     writeFunction "Arith.bc" mSomeFn'
 
