3a972dd4bef96b1224725fc87e42fb36f636f7e9
lennart <lennart@augustsson.net>
2009-01-20 23:32:59 +0000

    Add some file utilities.
    --HG--
    extra : convert_revision : 52b5530d28eed44223e22e5f0d9ec594fda6d620

---
+++ LLVM/Util/File.hs
@@ -0,0 +1,48 @@
+module LLVM.Util.File(writeCodeGenModule, optimizeFunction, optimizeFunctionCG) where
+import System.Directory
+import System.Process
+
+import LLVM.Core
+import LLVM.ExecutionEngine
+
+writeCodeGenModule :: String -> CodeGenModule a -> IO ()
+writeCodeGenModule name f = do
+    m <- newModule
+    defineModule m f
+    writeBitcodeToFile name m
+
+optimize :: String -> IO ()
+optimize name = do
+    _rc <- system $ "opt -std-compile-opts " ++ name ++ " -f -o " ++ name
+    return ()
+
+optimizeFunction :: (IsType t, Translatable t) => CodeGenModule (Function t) -> IO (Function t)
+optimizeFunction = fmap snd . optimizeFunction'
+
+optimizeFunction' :: (IsType t, Translatable t) => CodeGenModule (Function t) -> IO (Module, Function t)
+optimizeFunction' mdl = do
+    m <- newModule
+    mf <- defineModule m mdl
+    fName <- getValueName mf
+
+    let name = "__tmp__" ++ fName ++ ".bc"
+    writeBitcodeToFile name m
+
+    optimize name
+
+    m' <- readBitcodeFromFile name
+    funcs <- getModuleValues m'
+
+--    removeFile name
+
+    let Just mf' = castModuleValue =<< lookup fName funcs
+
+    return (m', mf')
+
+optimizeFunctionCG :: (IsType t, Translatable t) => CodeGenModule (Function t) -> IO t
+optimizeFunctionCG mdl = do
+    (m', mf') <- optimizeFunction' mdl
+    rf <- runEngineAccess $ do
+        addModule m'
+        generateFunction mf'
+    return rf
