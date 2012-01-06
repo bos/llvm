b37f17c18f24b266978dd1b24cdbb265d081ca6a
Bryan O'Sullivan <bos@serpentine.com>
2012-01-05 22:59:15 -0800

    Quiet another warning.

--- LLVM/Util/Arithmetic.hs
+++ LLVM/Util/Arithmetic.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# OPTIONS_GHC -fno-warn-orphans -fno-warn-warnings-deprecations #-}
 {-# LANGUAGE CPP, FlexibleInstances, ScopedTypeVariables, FlexibleContexts, UndecidableInstances, TypeSynonymInstances, MultiParamTypeClasses, FunctionalDependencies #-}
 module LLVM.Util.Arithmetic(
     TValue,
