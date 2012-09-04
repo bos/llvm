695aefd1648ea57adcba5527f000bb129af7fa04
Benjamin Saunders <besaunde@sfu.ca>
2012-09-04 13:25:54 -0700

    Module creation and disposal

--- wrapper/LLVM/Wrapper/Core.hs
+++ wrapper/LLVM/Wrapper/Core.hs
@@ -1,3 +1,23 @@
-module LLVM.Wrapper.Core where
+module LLVM.Wrapper.Core
+    (
+    -- ** Modules
+      moduleCreateWithName
+    , disposeModule
+    , withModule
+    ) where
+
+import Foreign.C.String
+import Control.Exception
 
 import LLVM.FFI.Core
+    ( ModuleRef
+    , disposeModule
+    )
+import qualified LLVM.FFI.Core as FFI
+
+moduleCreateWithName :: String -> IO ModuleRef
+moduleCreateWithName name = withCAString name FFI.moduleCreateWithName
+
+withModule :: String -> (ModuleRef -> IO a) -> IO a
+withModule n f = do m <- moduleCreateWithName n
+                    finally (f m) (disposeModule m)
