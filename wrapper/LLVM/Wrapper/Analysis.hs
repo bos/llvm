42eb60001ec121a72880f734b01761a94aa71d12
Benjamin Saunders <besaunde@sfu.ca>
2012-09-16 16:24:42 -0700

    Wrap verifyFunction and verifyModule

---
+++ wrapper/LLVM/Wrapper/Analysis.hs
@@ -0,0 +1,25 @@
+module LLVM.Wrapper.Analysis where
+
+import qualified LLVM.FFI.Analysis as FFI
+import qualified LLVM.FFI.Core as FFI
+import LLVM.Wrapper.Core
+
+import Foreign.C.String (peekCString)
+import Foreign.Marshal.Utils (toBool)
+import Foreign.Marshal.Alloc (alloca)
+import Foreign.Storable (peek)
+import Control.Monad (liftM)
+
+-- VerifierFailureAction 2 is 'no side effects'
+verifyFunction :: Value -> IO Bool
+verifyFunction f = fmap toBool $ FFI.verifyFunction f 2
+
+verifyModule :: Module -> IO (Maybe String)
+verifyModule m = alloca (\msgPtr -> do
+                           result <- FFI.verifyModule m 2 msgPtr
+                           msg <- peek msgPtr
+                           v <- case result of
+                                  0 -> return Nothing
+                                  _ -> fmap Just $ peekCString msg
+                           FFI.disposeMessage msg
+                           return v)
